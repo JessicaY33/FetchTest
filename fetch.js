654c6eb710e7e65791445a785222ba2c69d65fcc
Jessica <145158102+JessicaY33@users.noreply.github.com>
2023-11-30 15:33:57 -0500

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,55 @@
+//Task 1
+async function displayImg()
+{
+    const img = await fetch('https://source.unsplash.com/collection/190727/600x400');
+    const sender = await img.blob();
+    document.getElementById('food').src = URL.createObjectURL(sender);
+}
+
+displayImg().then(img =>{
+    console.log("Image displayed");
+})
+.catch(error =>{
+    console.log("Image error");
+})
+
+//Task 2
+async function arrayImg()
+{
+    const images = [4];
+    const container = document.getElementById('array');
+    for(let x = 0; x < 4; x++)
+    {
+        const img = await fetch('https://source.unsplash.com/collection/190727/600x400');
+        const sender = await img.blob();
+        images[x] = document.createElement('img');
+        images[x].src = URL.createObjectURL(sender);
+        container.appendChild(images[x]);
+    }
+}
+
+arrayImg().then(img =>{
+    console.log("Array displayed");
+})
+.catch(error =>{
+    console.log("Array error");
+})
+
+//https://dev.to/itsjjpowell/retrieving-files-with-the-fetch-api-i5l
+//Task 3
+async function writePoem()
+{
+    fetch('poem.txt')
+    .then(reponse => response.txt())
+    // const txt = await fetch('poem.txt');
+    // const sender = await txt.text();
+    // console.log(sender);
+    // document.getElementById("poetry").innerHTML = sender;
+}
+
+writePoem().then(text =>{
+    console.log("Text displayed");
+})
+.catch(error =>{
+    console.log("Text error");
+})
